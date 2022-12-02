2375797007ab18ce09e389b25ae8daf6e0cdc58a
Martin Hamáček <martin.hamacek@siemens.com>
2022-12-02 16:09:51 +0100

    2022/02v02

---
+++ 2022/Day02V02/RpsBase.cs
@@ -0,0 +1,17 @@
+﻿internal abstract class RpsBase
+{
+    protected int Win { get; } = 6;
+    protected int Draw { get; } = 3;
+    protected int Loss { get; } = 0;
+
+    protected int Rock { get; } = 1;
+    protected int Paper { get; } = 2;
+    protected int Sciss { get; } = 3;
+    protected Dictionary<string, int> _dict=new Dictionary<string, int>();
+
+    
+    public int GetPoints(string str)
+    {
+        return _dict[str];
+    }
+}
