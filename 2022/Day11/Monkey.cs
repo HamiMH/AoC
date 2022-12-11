162587ecc25962efdaf13d1b57f5c8212c9000e5
unknown <hamacekm93@gmail.com>
2022-12-11 20:37:26 +0100

    2022/11

---
+++ 2022/Day11/Monkey.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day11
+{
+
+
+
+    internal class Monkey
+    {
+        public static List<Monkey> MonkeyList = new List<Monkey>();
+
+        public static long Biggest2()
+        {
+            long big1 = 0;
+            long big2 = 0;
+            foreach (Monkey monkey in MonkeyList)
+            {
+                if (big1 > big2)
+                {
+                    if (monkey.NumberOfMoves > big2)
+                        big2 = monkey.NumberOfMoves;
+                }
+                else
+                {
+                    if (monkey.NumberOfMoves > big1)
+                        big1 = monkey.NumberOfMoves;
+                }
+            }
+            return big1 * big2;
+        }
+
+
+        public Queue<long> Items;
+
+        public Func<long, long> Operation;
+
+        public long TestValue;
+
+        public int TrueTarget;
+        public int FalseTarget;
+
+        public long NumberOfMoves = 0;
+
+        private long? _module=null;
+        private long ModuleValue
+        {
+            get
+            {
+                if (_module == null)
+                {
+                    _module = 1;
+                    foreach (Monkey monkey in MonkeyList)
+                        _module *= monkey.TestValue;
+                    return (long)_module;
+                }
+                else { return (long)_module; }
+            }
+        }
+
+        public Monkey(string inputStr)
+        {
+            Items = new Queue<long>();
+            string[] strArr = inputStr.Split(";");
+            string[] strArr1 = strArr[1].Replace("Starting items:", "").Trim().Split(",");
+            foreach (string str in strArr1)
+                Items.Enqueue(long.Parse(str.Trim()));
+
+            string strTmp = strArr[2].Replace("Operation: new = old", "").Trim();
+            char oper = strTmp[0];
+            if (strTmp.Substring(2) == "old")
+            {
+                if (oper == '+')
+                {
+                    Operation = (x => x + x);
+                }
+                else
+                {
+                    Operation = (x => x * x);
+                }
+            }
+            else
+            {
+                long val = long.Parse(strTmp.Substring(2));
+                if (oper == '+')
+                {
+                    Operation = (x => x + val);
+                }
+                else
+                {
+                    Operation = (x => x * val);
+                }
+            }
+
+            strTmp = strArr[3].Replace("Test: divisible by", "").Trim();
+            TestValue = long.Parse(strTmp);
+
+            strTmp = strArr[4].Replace("If true: throw to monkey", "").Trim();
+            TrueTarget = int.Parse(strTmp);
+
+            strTmp = strArr[5].Replace("If false: throw to monkey", "").Trim();
+            FalseTarget = int.Parse(strTmp);
+        }
+
+
+        public void PlayWithItems()
+        {
+            long item;
+            while (Items.Any())
+            {
+                item = Items.Dequeue();
+                item = Operation(item);
+                item = Mod(item);
+                if (item % TestValue == 0)
+                    MonkeyList[TrueTarget].Items.Enqueue(item);
+                else
+                    MonkeyList[FalseTarget].Items.Enqueue(item);
+                NumberOfMoves++;
+            }
+
+        }
+
+        private long Div3(long item)
+        {
+            long val = item / 3;
+            //if (item % 3 == 2)
+            //    val++;
+            return val;
+        }
+
+        private long Mod(long item)
+        {
+            return item% ModuleValue;
+        }
+    }
+}
---
+++ 2022/Day11/Program.cs
@@ -0,0 +1,84 @@
+﻿
+using Day11;
+using System.Diagnostics;
+using System.Diagnostics.Metrics;
+using System.Text;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<string> inputCol = new List<string>();
+        string lineIn1;
+        //while ((lineIn1 = Console.ReadLine()) != null)
+        while ((lineIn1 = Console.ReadLine()) != "eof")
+        {
+            //if (lineIn1 == "")
+            //    break;
+
+            inputCol.Add(lineIn1);
+        }
+        Stopwatch sw = new Stopwatch();
+        sw.Start();
+        //long result = GetResult1(inputCol);
+        long result = GetResult2(inputCol);
+        sw.Stop();
+
+        Console.WriteLine(result);
+        Console.WriteLine("Time was: " + sw.ElapsedMilliseconds + " ms.");
+    }
+
+    private static long GetResult1(List<string> inputCol)
+    {
+        StringBuilder stb= new StringBuilder();
+        foreach(string str in inputCol)
+        {
+            if(str=="" || str==String.Empty)
+            {
+                Monkey.MonkeyList.Add(new Monkey(stb.ToString()));
+                stb.Clear();
+                continue;
+            }
+            stb.Append(str);
+            stb.Append(";");
+        }
+        Monkey.MonkeyList.Add(new Monkey(stb.ToString()));
+        stb.Clear();
+
+        for (int i = 0; i < 20; i++)
+            foreach (Monkey monkey in Monkey.MonkeyList)
+                monkey.PlayWithItems();
+
+
+        return Monkey.Biggest2();
+    }
+
+
+    private static long GetResult2(List<string> inputCol)
+    {
+        StringBuilder stb = new StringBuilder();
+        foreach (string str in inputCol)
+        {
+            if (str == "" || str == String.Empty)
+            {
+                Monkey.MonkeyList.Add(new Monkey(stb.ToString()));
+                stb.Clear();
+                continue;
+            }
+            stb.Append(str);
+            stb.Append(";");
+        }
+        Monkey.MonkeyList.Add(new Monkey(stb.ToString()));
+        stb.Clear();
+
+        for (int i = 0; i < 10000; i++)
+            foreach (Monkey monkey in Monkey.MonkeyList)
+                monkey.PlayWithItems();
+
+
+        return Monkey.Biggest2();
+    }
+
+
+}
