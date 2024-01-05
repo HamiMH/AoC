3d44da5b5e979f2bac2a1654d40ae92d6f6622ea
Martin Hamáček <martin.hamacek@siemens.com>
2024-01-05 16:50:13 +0100

    hk

---
+++ 2020HK/Test/Test.hs
@@ -0,0 +1,29 @@
+   
+prLines :: (Show int, Show string, Num int, Num a2) => int -> [string] -> IO a2
+prLines i llines=
+                  if (not.null) llines 
+                    then                     
+                        do
+                            let str=show i
+                            writeFile "out" str
+                            let he=head llines
+                            writeFile "out" (show he)
+                            prLines (i+1) (tail llines)
+                            return 0                           
+                    else
+                            return 0                         
+
+
+main :: IO ()
+main = do
+        putStrLn "Hello World"
+        lline<- readFile "in"
+        writeFile "out" ""
+        let llines=lines lline
+        let a = prLines 1 llines
+        writeFile "out1" lline
+        --appendFile "out" "\n"
+        --appendFile "out" lline
+
+
+
